31c7189c10c111fae8531de4e5f85274975f85f5
Amb1tion <AAnsari1998@gmail.com>
2017-09-30 04:31:26 +0500

    added quote deletion

--- modules/quotes.py
+++ modules/quotes.py
@@ -55,6 +55,42 @@ class quotes():
                 num = await conn.fetchval('SELECT quote_num FROM servers WHERE serverid = $1',int(ctx.message.server.id))
                 await self.bot.say("Here's the latest of what the bright minds of this server have to offer: {} \nvalue: {}".format(dict[str(num-1)],str(num-1)))
 
+    @quote.command(pass_context=True)
+    @commands.cooldown(rate=1,per=5,type=commands.BucketType.channel)
+    @commands.has_permissions(manage_messages=True)
+    async def delete(self,ctx,index:int):
+        def check(message):
+            if message.content.lower() =='y':
+                return True
+            elif message.content.lower() =='n':
+                return False
+        async with self.pool.acquire() as conn:
+            chrysalis = await conn.fetchval('SELECT quotes FROM servers WHERE serverid=$1',int(ctx.message.server.id))
+            num = await conn.fetchval('SELECT quote_num FROM servers WHERE serverid=$1',int(ctx.message.server.id))
+            if chrysalis is None:
+                await self.bot.say('This server hasn\'t stored any quotes yet')
+            else:
+                try:
+                    dict = json.loads(chrysalis)
+                    if isinstance(index,int) and index in range(0 , num):
+                        await self.bot.say("Are you sure you want to delete(y/n): {}".format(dict[str(index)]))
+                        answer = await self.bot.wait_for_message(timeout=10.0, channel=ctx.message.channel, check=check)
+                        if answer is None:
+                            await self.bot.say('Operation timed out, make up your mind faster.')
+                        else:
+                            dict.pop(str(index))
+                            for id in range(index+1,num):
+                                dict[id-1]=dict[str(id)]
+                            dict.pop(str(num-1))
+                            await conn.execute('UPDATE servers SET quotes = $1 WHERE serverid = $2', json.dumps(dict),
+                                               int(ctx.message.server.id))
+                            await conn.execute('UPDATE servers SET quote_num=$1 WHERE serverid= $2', num-1,int(ctx.message.server.id))
+                            await self.bot.say("Quote deleted.")
+                    else:
+                        await self.bot.say("Index invalid or out of range.")
+                except Exception as e:
+                    print(e)
+
 
 
     @quote.command(pass_context=True)
