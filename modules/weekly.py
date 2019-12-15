1780825b1583aa0eab2a5d6c78d54a339d365c4e
Amb1tion <AAnsari1998@gmail.com>
2019-12-16 01:22:14 +0500

    added yearly charts

--- modules/weekly.py
+++ modules/weekly.py
@@ -86,6 +86,46 @@ class weekly():
 
 	@commands.command(pass_context=True)
 	@commands.cooldown(rate=1.0, per=5.0, type=commands.BucketType.channel)
+	async def yearly(self, ctx, *, args=None):
+		await self.bot.send_typing(ctx.message.channel)
+		Regex = re.compile(r'([A-Za-z0-9_\-]{2,15})?\s?([1-9]x[1-9])$')
+		try:
+			mo = Regex.search(args)
+			if mo is None:
+				await self.bot.say(
+					'You\'re passing invalid args, do `prefix weekly username [3x3,4x4,5x5,2x6] or `prefix weekly` if you have your last.fm username set')
+			if mo.group(1):
+				user = mo.group(1)
+			else:
+				async with self.pool.acquire() as conn:
+					user = await conn.fetchval('SELECT lastfm FROM users WHERE userid = $1', int(ctx.message.author.id))
+
+			if mo.group(2) in ['3x3', '4x4', '5x5', '2x6']:
+				size = mo.group(2)
+			else:
+				await self.bot.say('That\'s an invalid format, try one of these (3x3,4x4,5x5,2x6)')
+		except TypeError:
+			try:
+				async with self.pool.acquire() as conn:
+					user = await conn.fetchval('SELECT lastfm FROM users WHERE userid = $1', int(ctx.message.author.id))
+				if user is None:
+					raise AttributeError
+				else:
+					size='3x3'
+
+			except:
+				await self.bot.say('You have to submit your patrician account using `!fm set usernamehere`')
+		link = "http://tapmusic.net/collage.php?user=" + user + "&type=12month&size=" + size + "&caption=true"
+
+		with aiohttp.ClientSession() as session:
+			async with session.get(link) as resp:
+				var = await resp.read()
+				f = io.BytesIO(var)
+		await self.bot.send_file(ctx.message.channel, f, filename='image.png',
+								 content=ctx.message.author.mention + ' here\'s your chart')
+
+	@commands.command(pass_context=True)
+	@commands.cooldown(rate=1.0, per=5.0, type=commands.BucketType.channel)
 	async def alltime(self, ctx, *, args=None):
 		await self.bot.send_typing(ctx.message.channel)
 		Regex = re.compile(r'([A-Za-z0-9_\-]{2,15})?\s?([1-9]x[1-9])$')
