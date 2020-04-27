4922e402fed26802b19385de07d92037182429ce
Amb1tion <AAnsari1998@gmail.com>
2020-04-27 17:28:41 +0500

    fixed mobile hyperlink embed

--- modules/youtube.py
+++ modules/youtube.py
@@ -6,7 +6,7 @@ config.read('bot_config.ini')
 API_KEY = config['keys']['youtube_key']
 
 
-class YoutubeCog(commands.Cog):
+class Youtube(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.payload = {} #defining dict to use for paramater passing
@@ -27,11 +27,11 @@ class YoutubeCog(commands.Cog):
                 async with session.get("https://www.googleapis.com/youtube/v3/search?",params=payload) as r:
                     data = await r.json()
                     link = "https://www.youtube.com/watch?v={}".format(data['items'][0]['id']['videoId'])
-                    await ctx.message.channel.send(link)
+                    await ctx.send(link)
         except Exception as E:
-            await ctx.message.channel.send(E)
+            await ctx.send(E)
 
 
 
 def setup(bot):
-    bot.add_cog(YoutubeCog(bot))
+    bot.add_cog(Youtube(bot))
